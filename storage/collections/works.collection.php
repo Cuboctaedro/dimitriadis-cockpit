ecbad509e02b1bac20c3e2b4b8dfeed2c6f8f189
Dimitris Kottas <info@cuboctaedro.eu>
2020-03-06 18:42:42 +0200

    first commit

---
+++ storage/collections/works.collection.php
@@ -0,0 +1,171 @@
+<?php
+ return array (
+  'name' => 'works',
+  'label' => 'Works',
+  '_id' => 'works5e6279166473b',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'title',
+      'label' => 'Title',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+        'slug' => true,
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => true,
+    ),
+    1 => 
+    array (
+      'name' => 'place',
+      'label' => 'Place',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => false,
+    ),
+    2 => 
+    array (
+      'name' => 'date',
+      'label' => 'Date',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => false,
+    ),
+    3 => 
+    array (
+      'name' => 'medium',
+      'label' => 'Medium',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => false,
+    ),
+    4 => 
+    array (
+      'name' => 'size',
+      'label' => 'Size',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => false,
+    ),
+    5 => 
+    array (
+      'name' => 'cover',
+      'label' => 'Cover',
+      'type' => 'image',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => false,
+    ),
+    6 => 
+    array (
+      'name' => 'gallery',
+      'label' => 'Images',
+      'type' => 'gallery',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => false,
+    ),
+  ),
+  'sortable' => true,
+  'in_menu' => false,
+  '_created' => 1583511830,
+  '_modified' => 1583511830,
+  'color' => '#5D9CEC',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+      'enabled' => false,
+    ),
+    'read' => 
+    array (
+      'enabled' => false,
+    ),
+    'update' => 
+    array (
+      'enabled' => false,
+    ),
+    'delete' => 
+    array (
+      'enabled' => false,
+    ),
+  ),
+  'icon' => 'gallery.svg',
+);
